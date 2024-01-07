200836bbafecf1a0a16a100a7cba2f17ca1fad00
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2024-01-07 14:26:50 -0300

    Ajustando os projetos com alguns feitos no passado e começando a usar os conceitos de MVC

---
+++ PersonController/Controllers/PersonController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculadoraRest.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+
+    public class PersonController : ControllerBase {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber) {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDouble(string strNumber) {
+            double doubleValue;
+            if (double.TryParse(strNumber, out doubleValue)) {
+                return doubleValue;
+            }
+            return 0;
+        }
+
+        private decimal ConvertToDecimal(string strNumber) {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue)) {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber) {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
+
+
